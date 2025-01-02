77068f48b7ebd8e64c16dbec2731d0e3add971ca
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-01-02 16:58:44 +0600

    welcome

--- lib/pages/welcome/welcomepage.dart
+++ lib/pages/welcome/welcomepage.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {

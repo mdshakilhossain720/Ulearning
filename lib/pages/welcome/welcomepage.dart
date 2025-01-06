e0497239bc7dd79f2b00d5ee157ee9c923df3bdd
shakil <mdshakilhossain720@gmail.com>
2025-01-06 20:38:39 +0600

    fix

--- lib/pages/welcome/welcomepage.dart
+++ lib/pages/welcome/welcomepage.dart
@@ -12,39 +12,7 @@ class WelcomeScreen extends StatelessWidget {
           PageView(
             scrollDirection: Axis.horizontal,
             children: [
-              Column(
-                children: [
-                  Image.asset(
-                    "assets/images/reading.png",
-                    fit: BoxFit.fitWidth,
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 15),
-                    child: const Text(
-                      "Welcome to U Learning",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top: 15),
-                    padding: const EdgeInsets.only(left: 20, right: 20),
-                    child: const Text(
-                      "Welcome to U Learning",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  )
-                ],
-              )
+              appOnBoarding(),
             ],
           ),
           const Positioned(child: Text("jbcv c")),
